af922c0c4e96effcd2e7c448c84a800d9e1a17bf
Bich Nhuy <163.nhuy@gmail.com>
2020-10-23 22:59:06 +0700

    2.0.0

--- BTGit2/Source.cpp
+++ BTGit2/Source.cpp
@@ -16,7 +16,22 @@ int main()
 	//bai 1	
 		
 	case 2:
-	//bai 2
+	 int a,b;
+    cout << "-----------Tinh DT HCN----------\n";
+    cout << "nhap vao 2 canh a, b: " ;
+    cin >> a>>b;
+    cout << "Dien tich hcn : " << a*b;
+ 
+    cout << "\n-----------Tinh DT HV----------\n";
+    cout << "nhap vao 2 canh a = " ;
+    cin >> a;
+    cout << "Dien tich hinh vuong : " << a*a;
+ 
+    cout << "\n-----------Tinh DT HT----------\n";
+    float r;
+    cout << "nhap vao ban kinh r = " ;
+    cin >> r;
+    cout << "dien tic hinh tron la : " << r*r*pi;
 	case 3:
 	//bai 3
 	case 4:// Bai 4		
