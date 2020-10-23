0a92ffde4f836e56558020488e4f5fa09f0497e1
Nguyen Thanh Long <thanh.long.ntl.1999@gmail.com>
2020-10-23 20:21:51 +0700

    first commit

---
+++ BTGit2/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char baiTap2;
+	switch (baiTap2)
+	{
+	case 1:// bai 
+		
+		
+	case 2:
+		
+	case 3:
+		
+	case 4:// Long
+	
+
+	default:
+		cout << "Chon sai phep toan";
+		break;
+	}
+	return 0;
+}
