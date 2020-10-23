7df2faf29f1d4ec325557654594f0da3e2a37f9f
Nguyen Thanh Long <thanh.long.ntl.1999@gmail.com>
2020-10-23 20:51:24 +0700

    3.1.0

--- BTGit2/Source.cpp
+++ BTGit2/Source.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main()
 {
-	char baiTap2;
+	srand(time(NULL));
+	int baiTap2;
+	cout << "Nhap lua chon: ";
+	cin >> baiTap2;
+
 	switch (baiTap2)
 	{
 	case 1:// bai 
@@ -13,12 +19,19 @@ int main()
 		
 	case 3:
 		
-	case 4:// Long
-	
+	case 4:// Long		
+		int a;
+		a = rand() % (100) + 1;
+		int b;
+		b = rand() % (100) + 1;
+		cout <<a<<" - "<<b<<" = "<< a - b;
+
+		break;
 
 	default:
 		cout << "Chon sai phep toan";
 		break;
 	}
 	return 0;
+	
 }
